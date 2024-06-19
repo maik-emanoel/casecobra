bf95b5d68e2f86ddefa549d65acd82afb19f1d92
Maik Emanoel <maikemanoel11@gmail.com>
2024-06-19 20:56:40 -0300

    feat: create DesignPage

---
+++ src/app/configure/design/page.tsx
@@ -0,0 +1,36 @@
+import { notFound } from 'next/navigation'
+import { db } from '@/db'
+
+import { DesignConfigurator } from './design-configurator'
+
+interface DesignPageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+
+export default async function DesignPage({ searchParams }: DesignPageProps) {
+  const { id } = searchParams
+
+  if (!id || typeof id !== 'string') {
+    return notFound()
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  })
+
+  if (!configuration) {
+    return notFound()
+  }
+
+  const { imageUrl, width, height } = configuration
+
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageUrl={imageUrl}
+      imageDimensions={{ width, height }}
+    />
+  )
+}
