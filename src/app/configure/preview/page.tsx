70f5c5995fd705b1ca12a13449dcb8dcf3e8b1d5
Maik Emanoel <maikemanoel11@gmail.com>
2024-06-20 15:52:52 -0300

    feat: create PreviewPage

---
+++ src/app/configure/preview/page.tsx
@@ -0,0 +1,28 @@
+import { notFound } from 'next/navigation'
+
+import { db } from '@/db'
+import { DesignPreview } from './design-preview'
+
+interface PreviewPageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+
+export default async function PreviewPage({ searchParams }: PreviewPageProps) {
+  const { id } = searchParams
+
+  if (!id || typeof id !== 'string') {
+    return notFound()
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  })
+
+  if (!configuration) {
+    return notFound()
+  }
+
+  return <DesignPreview configuration={configuration} />
+}
