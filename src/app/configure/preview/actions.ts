c1c9275d8d4d33ccc25b83a5f535e72d0e8be1ce
Maik Emanoel <maikemanoel11@gmail.com>
2024-06-21 11:17:36 -0300

    feat: handle with checkout session

---
+++ src/app/configure/preview/actions.ts
@@ -0,0 +1,93 @@
+'use server'
+
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+
+import { db } from '@/db'
+import { BASE_PRICE, PRODUCT_PRICES } from '@/config/products'
+import { Order } from '@prisma/client'
+import { stripe } from '@/lib/stripe'
+
+export async function createCheckoutSession({
+  configId,
+}: {
+  configId: string
+}) {
+  const configuration = await db.configuration.findUnique({
+    where: { id: configId },
+  })
+
+  if (!configuration) {
+    throw new Error('No such configuration found')
+  }
+
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  if (!user) {
+    throw new Error('You need to be logged in')
+  }
+
+  const { finish, material } = configuration
+  let price = BASE_PRICE
+
+  if (finish === 'textured') {
+    price += PRODUCT_PRICES.finish.textured
+  }
+
+  if (material === 'polycarbonate') {
+    price += PRODUCT_PRICES.material.polycarbonate
+  }
+
+  let order: Order | undefined = undefined
+
+  console.log(user.id, configuration.id)
+  const existingOrder = await db.order.findFirst({
+    where: {
+      userId: user.id,
+      configurationId: configuration.id,
+    },
+  })
+
+  if (existingOrder) {
+    order = existingOrder
+  } else {
+    order = await db.order.create({
+      data: {
+        amount: price / 100,
+        userId: user.id,
+        configurationId: configuration.id,
+      },
+    })
+  }
+
+  const product = await stripe.products.create({
+    name: 'Custom iPhone Case',
+    images: [configuration.imageUrl],
+    default_price_data: {
+      currency: 'USD',
+      unit_amount: price,
+    },
+  })
+
+  const stripeSession = await stripe.checkout.sessions.create({
+    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
+    payment_method_types: ['pix', 'card', 'paypal'],
+    mode: 'payment',
+    shipping_address_collection: {
+      allowed_countries: ['BR'],
+    },
+    metadata: {
+      userId: user.id,
+      orderId: order.id,
+    },
+    line_items: [
+      {
+        price: product.default_price as string,
+        quantity: 1,
+      },
+    ],
+  })
+
+  return { url: stripeSession.url }
+}
