1cac4c9678606af207e4742f1dd8ac0e273e48ab
Maik Emanoel <maikemanoel11@gmail.com>
2024-06-25 10:15:54 -0300

    refactor: remove some payment_method_types

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -72,7 +72,7 @@ export async function createCheckoutSession({
   const stripeSession = await stripe.checkout.sessions.create({
     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
-    payment_method_types: ['pix', 'card', 'paypal'],
+    payment_method_types: ['card'],
     mode: 'payment',
     shipping_address_collection: {
       allowed_countries: ['BR'],
