05e240b29b6be515d69a5dab39589151e119da7c
Maik Emanoel <maikemanoel11@gmail.com>
2024-06-27 12:04:34 -0300

    feat: handle resend config

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,9 +1,13 @@
 import { NextResponse } from 'next/server'
 import { headers } from 'next/headers'
+import { Resend } from 'resend'
 import Stripe from 'stripe'
 
 import { stripe } from '@/lib/stripe'
 import { db } from '@/db'
+import { OrderReceivedEmail } from '@/components/emails/order-received-order'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req: Request) {
   try {
@@ -41,7 +45,7 @@ export async function POST(req: Request) {
       const billingAddress = session.customer_details!.address
       const shippingAddress = session.shipping_details!.address
 
-      await db.order.update({
+      const updatedOrder = await db.order.update({
         where: {
           id: orderId,
         },
@@ -68,6 +72,25 @@ export async function POST(req: Request) {
             },
           },
         },
+      })
+
+      await resend.emails.send({
+        from: 'CaseCobra <contato.maikemanoel@gmail.com>',
+        to: [event.data.object.customer_details.email],
+        subject: 'Thanks for your order!',
+        react: OrderReceivedEmail({
+          orderId,
+          orderDate: updatedOrder.createdAt.toLocaleDateString(),
+          // @ts-ignore
+          shippingAddress: {
+            name: session.customer_details!.name!,
+            city: shippingAddress!.city!,
+            country: shippingAddress!.country!,
+            postalCode: shippingAddress!.postal_code!,
+            street: shippingAddress!.line1!,
+            state: shippingAddress!.state,
+          },
+        }),
       })
     }
 
